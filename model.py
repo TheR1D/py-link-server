9a419afec845b38d847c4f1907fb5be3d3cd5ecc
Farkhod Sadykov <farkhod.sadykov@gmail.com>
2022-07-05 22:45:10 +0200

    Fixing bug with id on Link model. The default value of id field was assigned to constant value.

--- model.py
+++ model.py
@@ -4,12 +4,15 @@ from urllib.parse import urlparse
 from sqlalchemy.orm import validates
 from config import database, marshmallow
 
+def random_token():
+    return token_urlsafe(nbytes=4)
+
 
 class Link(database.Model):
     def __repr__(self):
         return ", ".join("%s: %s" % item for item in vars(self).items())
 
-    id = database.Column(database.String, primary_key=True, default=token_urlsafe(nbytes=4))
+    id = database.Column(database.String, primary_key=True, default=random_token)
     original_url = database.Column(database.String(256), nullable=False)
     created_at = database.Column(database.DateTime, default=datetime.now(), nullable=False)
     view_count = database.Column(database.Integer, default=0, nullable=False)
